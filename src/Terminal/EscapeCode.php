4b48189688c8f1bb36021e448a7cf7afa1b92ab9
Petr Trofimov <petrofimov@yandex.ru>
2013-03-27 00:17:16 +0300

    escapecode: added some methods

--- src/Terminal/EscapeCode.php
+++ src/Terminal/EscapeCode.php
@@ -1,6 +1,10 @@
 <?php
 namespace Terminal;
 
+/**
+ * @see http://invisible-island.net/xterm/ctlseqs/ctlseqs.html
+ * @see http://ascii-table.com/ansi-escape-sequences.php
+ */
 class EscapeCode
 {
     public function smcup()
@@ -11,5 +15,22 @@ class EscapeCode
     public function rmcup()
     {
         return "\033[?1049l";
+    }
+
+    /**
+     * Set/reset auto-wrap mode
+     *
+     * @param bool $flag
+     *
+     * @return string
+     */
+    public function autoWrap($flag)
+    {
+        return $flag ? "\033[?7h" : "\033[?7l";
+    }
+
+    public function screenSize()
+    {
+        return "\033[18t";
     }
 }
